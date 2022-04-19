4564b0df7070745fd3882583c5d56da7cc8d8751
Subhi YD <subhy809fr@gmail.com>
2022-04-19 15:20:52 +0200

    V6.1 Rajout de commentaires dans HelloWorldApplication

--- src/main/java/com/openclassrooms/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/openclassrooms/HelloWorld/HelloWorldApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication implements CommandLineRunner {
+	/*
+	 C’est grâce à l’IoC container de Spring, l’injection de dépendances. En mettant l’annotation @Autowired sur l’attribut bs,
+	 Spring va chercher au sein de son contexte s’il existe un bean de type BusinessService.
+	 S’il le trouve, il va alors instancier la classe de ce bean et injecter cette instance dans l’attribut.
+	  S’il ne trouve pas de bean de ce type, Spring génère une erreur.*/
 	@Autowired
 	private BusinessService bs;
 
